d85562b3c8eb3f998d5ea2b19cae8677d5c9fb4e
TNTStudios <tntstudiosn@gmail.com>
2025-06-07 23:35:04 -0600

    Initial commit

---
+++ src/client/java/com/TNTStudios/vicefinal/client/VicefinalClient.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.vicefinal.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VicefinalClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/TNTStudios/vicefinal/Vicefinal.java
@@ -0,0 +1,10 @@
+package com.TNTStudios.vicefinal;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Vicefinal implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
